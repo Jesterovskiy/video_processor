ab411806f6d21eab803d79016583ee9390cb369e
Jester <jester.dash@gmail.com>
2016-12-15 13:21:10 +0200

    Add download dir

--- config/config.exs
+++ config/config.exs
@@ -29,7 +29,8 @@ config :video_processor,
   uplynk_account_guid: {:system, "UPLYNK_ACCOUNT_GUID"},
   uplynk_secret_key:   {:system, "UPLYNK_SECRET_KEY"},
   s3_url:              {:system, "AWS_S3_URL"},
-  disk_storage:        {:system, "DISK_STORAGE"}
+  disk_storage:        {:system, "DISK_STORAGE"},
+  download_dir:        {:system, "DOWNLOAD_DIR"}
 config :ex_aws,
   access_key_id:     {:system, "AWS_ACCESS_KEY_ID"},
   secret_access_key: {:system, "AWS_SECRET_ACCESS_KEY"},
--- lib/video_processor/download.ex
+++ lib/video_processor/download.ex
@@ -41,10 +41,11 @@ defmodule VideoProcessor.Download do
   end
 
   def download(src, output_filename) do
-    IO.puts "Downloading #{src} -> #{output_filename}"
+    download_dir = Confex.get(:video_processor, :download_dir)
+    IO.puts "Downloading #{src} -> #{download_dir <> output_filename}"
     body = HTTPoison.get!(src).body
-    File.write!(output_filename, body)
-    IO.puts "Done Downloading #{src} -> #{output_filename}"
+    File.write!(download_dir <> output_filename, body)
+    IO.puts "Done Downloading #{src} -> #{download_dir <> output_filename}"
     GenServer.cast(VideoProcessor.Download, {:download_finish, output_filename})
   end
 end
