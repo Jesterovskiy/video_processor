a69ea4459bd7742360304d5bc081797bd0b2d879
Jester <jester.dash@gmail.com>
2017-01-24 03:37:49 +0200

    Update version and elixir

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule VideoProcessor.Mixfile do
 
   def project do
     [app: :video_processor,
-     version: "0.1.0",
-     elixir: "~> 1.3",
+     version: "0.2.0",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
